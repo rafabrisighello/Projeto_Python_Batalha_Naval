4d87b9992091917f8cdbc6488fbf6379516a7fec
rafabrisighello <rafael.ricciardi@yahoo.com.br>
2021-10-19 13:34:41 -0300

    interface de entrada de dados quase completa

--- batalha_naval.py
+++ batalha_naval.py
@@ -9,11 +9,11 @@ cont = False
 # Ship dictionary
 
 ship_dic = {'Carrier':[5,1], 'Battleship': [4,2], 'Cruiser': [3,3], 'Destroyer': [2,4]}
-ship_kind = {1:'Carrier', 2:'Battleship', 3:'Cruiser', 4:'Destroyer'}
+ship_kind = {1: 'Carrier', 2: 'Battleship', 3:'Cruiser', 4:'Destroyer'}
 
 ships_to_place = [ship_dic, ship_dic]
 
-ships_set = [{}, {}]
+ships_set1 = [{}, {}]
 
 # Grid initialization
 
@@ -47,7 +47,7 @@ def grid_render(grid_item):
 def ship_placer(grid_item, ship, start_row, start_col, orientation):
     cor_dic = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10}
     start_col_int = cor_dic[start_col]
-    ship_size = ship_dic[ship][0]
+    ship_size = 6 - ship
     
     if (start_row < 1 or start_row > 10 or start_col_int < 1 or start_col_int > 10):
         cont = False
@@ -86,33 +86,73 @@ def ship_placer(grid_item, ship, start_row, start_col, orientation):
 def player_set_loop(player):
     print('Player ' + str(player) + ', arrange your ships in the game board!\n')
     while not(p_set[player - 1]):
-        print('Options remaining for player ' + str(player) + ': Kind of ship: [size, quantity] \n')
+        print('Options remaining for player ' + str(player) + ': Kind of ship: [size, quantity, option index] \n')
         print('Your options are: \n')
         print(ships_to_place[player - 1])
-        ship_code = int(input('\nSelect your ship kind according to the options above. Type 1 for Carrier, 2 for Battleship, 3 for Cruiser, 4 for Destroyer\n'))
+        ship_code = select_ship(ships_to_place[player - 1])
         grid_render(grid)
-        ship_start_row = int(input('\nSelect your starting row according to the grid\n'))
-        ship_start_col_raw = input('\nSelect your starting column according to the grid\n')
-        ship_start_col_trim = ship_start_col_raw.upper()
-        ship_orientation_raw = input('\n Set your ship orientation: type V for vertical ou H for horizontal\n')
-        ship_orientation_trim = ship_orientation_raw.upper()
-        ship_placer(grid_P1, ship_kind[ship_code], ship_start_row, ship_start_col_trim, ship_orientation_trim)
+        row = select_row()
+        col = select_column()
+        orientation = select_orientation()
+        ship_placer(grid_P1, ship_code, row, col, orientation)
         grid_render(grid_P1)
         ships_to_place[player - 1][ship_kind[ship_code]][1] -= 1
         print(ships_to_place[player - 1])
         if ships_to_place[player - 1][ship_kind[ship_code]][1] == 0:
-            ships_set[player - 1] = ships_to_place[player - 1].pop(ship_kind[ship_code])
-            print(ships_set[player - 1])
+            ships_set1[player - 1] = ships_to_place[player - 1].pop(ship_kind[ship_code])
+            print(ships_set1[player - 1])
             if len(ships_to_place[player - 1]) == 0:
                 p_set[player - 1] = True
 
 
-# Loop de jogo principal
+def select_ship(ships_to_place):
+    ship_code = 0
+    choices = []
+    for ship in ships_to_place:
+        for key in ship_kind:
+            if ship == ship_kind[key]:
+                choices.append(key)
+    while ship_code not in choices:
+        print('Your available choices are: ')
+        print(choices)
+        ship_code = int(input('\nSelect your ship kind according to the options above. Type 1 for Carrier, 2 for Battleship, 3 for Cruiser, 4 for Destroyer\n'))
+    return ship_code
 
-print('Welcome to Battleship!!\n')
+def select_row():
+    row = 0
+    choices = list(range(1,11))
+    while row not in choices:
+        row = int(input('\nSelect your starting row according to the grid\n'))
+    return row
+
+def select_column():
+    col = 0
+    choices = ['A','B','C','D','E','F','G','H','I','J']
+    while col not in choices:
+        col = input('\nSelect your starting column according to the grid\n').upper()
+    return col
+
+def select_orientation():
+    orientation = ''
+    choices = ['V','H']
+    while orientation not in choices:
+        orientation = input('Set your ship orientation: type V for vertical ou H for horizontal\n').upper()
+    return orientation
+
+def main_game_loop(player):
+    pass
+
+
+# Loop de jogo principal
+print('\n=======================')
+print('Welcome to Battleship!!')
+print('=======================\n')
 
 player_set_loop(1)
 player_set_loop(2)
+
+n = 9 // 10
+print(n)
 
 
 
